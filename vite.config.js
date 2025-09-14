ac2be040c3bae74b26f35abcd5dc2ee4bb039842
Myanex <142110964+Myanex@users.noreply.github.com>
2025-09-14 17:40:30 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Si falla por el plugin, quita la línea "plugins: [react()]"
-export default defineConfig({
-  plugins: [react()]
-})
