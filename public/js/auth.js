954a3de1e05d49c5e5b3d3bbccd47ed14ef71eeb
Myanex <142110964+Myanex@users.noreply.github.com>
2025-09-16 11:59:46 -0300

    Create auth.js

---
+++ public/js/auth.js
@@ -0,0 +1,25 @@
+import { supabase, whoami } from "./supabaseClient.js";
+
+export async function checkSession() {
+  const { data: { session } } = await supabase.auth.getSession();
+  if (session) {
+    document.querySelector("#view-auth").classList.remove("active");
+    document.querySelector("#view-app").classList.add("active");
+    return await whoami();
+  } else {
+    document.querySelector("#view-auth").classList.add("active");
+    document.querySelector("#view-app").classList.remove("active");
+    return null;
+  }
+}
+
+export async function login(email, password) {
+  const { error } = await supabase.auth.signInWithPassword({ email, password });
+  if (error) throw error;
+  return await checkSession();
+}
+
+export async function logout() {
+  await supabase.auth.signOut();
+  location.reload();
+}
