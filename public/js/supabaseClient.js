26c51ac42ee59eef744675eb3faa694adddc3c57
Myanex <142110964+Myanex@users.noreply.github.com>
2025-09-18 14:28:21 -0300

    Use ESM Supabase client

--- public/js/supabaseClient.js
+++ public/js/supabaseClient.js
@@ -1,6 +1,7 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import { ENV } from "../env.js";
 
-export const supabase = window.supabase.createClient(
+export const supabase = createClient(
   ENV.SUPABASE_URL,
   ENV.SUPABASE_ANON_KEY
 );
