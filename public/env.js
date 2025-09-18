d790acae66bd05f1a95e09304e7eb550f2dedfab
Myanex <142110964+Myanex@users.noreply.github.com>
2025-09-18 15:50:20 -0300

    Delete public directory

--- public/env.js
+++
@@ -1,6 +0,0 @@
-export const ENV = {
-  SUPABASE_URL: "https://gylndpvsjifxdlzcqqmh.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5bG5kcHZzamlmeGRsemNxcW1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4NTQyNjUsImV4cCI6MjA3MzQzMDI2NX0.REi4s98xlTYDKNj5nuaiZuosQEuBt42SuAiWNlm5d_8"
-};
-
-
